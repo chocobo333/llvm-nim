7474070534fcc62b99f218841d43d85b5f64a5db
chocobo333 <chocobo1355@gmail.com>
2020-05-31 23:29:20 +0900

    revise Comdat.nim

--- src/llvm/Comdat.nim
+++ src/llvm/Comdat.nim
@@ -11,52 +11,48 @@
 ## |*                                                                            *|
 ## \*===----------------------------------------------------------------------===
 
+import prelude/platforms
+
+import Types
+
+
 type
     ComdatSelectionKind* {.size: sizeof(cint).} = enum
-        AnyComdatSelectionKind,     ## /< The linker may choose any COMDAT.
-        ExactMatchComdatSelectionKind, ## /< The data referenced by the COMDAT must
-                                    ## /< be the same.
-        LargestComdatSelectionKind, ## /< The linker will choose the largest
-                                    ## /< COMDAT.
-        NoDuplicatesComdatSelectionKind, ## /< No other Module may specify this
-                                    ## /< COMDAT.
-        SameSizeComdatSelectionKind ## /< The data referenced by the COMDAT must be
-                                    ## /< the same size.
+        AnyComdatSelectionKind              ## /< The linker may choose any COMDAT.
+        ExactMatchComdatSelectionKind       ## /< The data referenced by the COMDAT must
+                                            ## /< be the same.
+        LargestComdatSelectionKind          ## /< The linker will choose the largest
+                                            ## /< COMDAT.
+        NoDuplicatesComdatSelectionKind     ## /< No other Module may specify this
+                                            ## /< COMDAT.
+        SameSizeComdatSelectionKind         ## /< The data referenced by the COMDAT must be
+                                            ## /< the same size.
 
 
-## *
-##  Return the Comdat in the module with the specified name. It is created
-##  if it didn't already exist.
-##
-##  @see llvm::Module::getOrInsertComdat()
-##
 
 proc getOrInsertComdat*(m: ModuleRef; name: cstring): ComdatRef {.cdecl, importc: "LLVMGetOrInsertComdat", dynlib: LLVMlib.}
-## *
-##  Get the Comdat assigned to the given global object.
-##
-##  @see llvm::GlobalObject::getComdat()
-##
+    ##  Return the Comdat in the module with the specified name. It is created
+    ##  if it didn't already exist.
+    ##
+    ##  @see llvm::Module::getOrInsertComdat()
 
 proc getComdat*(v: ValueRef): ComdatRef {.cdecl, importc: "LLVMGetComdat", dynlib: LLVMlib.}
-## *
-##  Assign the Comdat to the given global object.
-##
-##  @see llvm::GlobalObject::setComdat()
-##
+    ##  Get the Comdat assigned to the given global object.
+    ##
+    ##  @see llvm::GlobalObject::getComdat()
 
 proc setComdat*(v: ValueRef; c: ComdatRef) {.cdecl, importc: "LLVMSetComdat", dynlib: LLVMlib.}
-##
-##  Get the conflict resolution selection kind for the Comdat.
-##
-##  @see llvm::Comdat::getSelectionKind()
-##
+    ##  Assign the Comdat to the given global object.
+    ##
+    ##  @see llvm::GlobalObject::setComdat()
 
 proc getComdatSelectionKind*(c: ComdatRef): ComdatSelectionKind {.cdecl, importc: "LLVMGetComdatSelectionKind", dynlib: LLVMlib.}
-##
-##  Set the conflict resolution selection kind for the Comdat.
-##
-##  @see llvm::Comdat::setSelectionKind()
-##
+    ##  Get the conflict resolution selection kind for the Comdat.
+    ##
+    ##  @see llvm::Comdat::getSelectionKind()
+
 
 proc setComdatSelectionKind*(c: ComdatRef; kind: ComdatSelectionKind) {.cdecl, importc: "LLVMSetComdatSelectionKind", dynlib: LLVMlib.}
+    ##  Set the conflict resolution selection kind for the Comdat.
+    ##
+    ##  @see llvm::Comdat::setSelectionKind()
