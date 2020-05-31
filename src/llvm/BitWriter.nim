66f83199928863f371ec7bfaa13628dbd8d6655a
chocobo333 <chocobo1355@gmail.com>
2020-05-31 23:26:50 +0900

    revise BitWriter.nim

--- src/llvm/BitWriter.nim
+++ src/llvm/BitWriter.nim
@@ -15,26 +15,23 @@
 ## |* tools written in such languages.                                           *|
 ## |*                                                                            *|
 ## \*===----------------------------------------------------------------------===
-## *
-##  @defgroup LLVMCBitWriter Bit Writer
-##  @ingroup LLVMC
-##
-##  @{
-##
+
+import prelude/platforms
+
+import Types
+
+
 ## ===-- Operations on modules ---------------------------------------------===
-## * Writes a module to the specified path. Returns 0 on success.
 
 proc writeBitcodeToFile*(m: ModuleRef; path: cstring): cint {.cdecl, importc: "LLVMWriteBitcodeToFile", dynlib: LLVMlib.}
-## * Writes a module to an open file descriptor. Returns 0 on success.
+    ## * Writes a module to the specified path. Returns 0 on success.
 
 proc writeBitcodeToFD*(m: ModuleRef; fd: cint; shouldClose: cint;unbuffered: cint): cint {. cdecl, importc: "LLVMWriteBitcodeToFD", dynlib: LLVMlib.}
-## * Deprecated for LLVMWriteBitcodeToFD. Writes a module to an open file
-##     descriptor. Returns 0 on success. Closes the Handle.
+    ## * Writes a module to an open file descriptor. Returns 0 on success.
 
 proc writeBitcodeToFileHandle*(m: ModuleRef; handle: cint): cint {.cdecl, importc: "LLVMWriteBitcodeToFileHandle", dynlib: LLVMlib.}
-## * Writes a module to a new memory buffer and returns it.
+    ## * Deprecated for LLVMWriteBitcodeToFD. Writes a module to an open file
+    ##     descriptor. Returns 0 on success. Closes the Handle.
 
 proc writeBitcodeToMemoryBuffer*(m: ModuleRef): MemoryBufferRef {.cdecl, importc: "LLVMWriteBitcodeToMemoryBuffer", dynlib: LLVMlib.}
-## *
-##  @}
-##
+    ## * Writes a module to a new memory buffer and returns it.
