afc3e4d102443f0a43adad43c69496748eb3b8f3
chocobo333 <chocobo1355@gmail.com>
2020-05-31 23:22:13 +0900

    revise Analysis.nim

--- src/llvm/Analysis.nim
+++ src/llvm/Analysis.nim
@@ -15,12 +15,11 @@
 ## |* tools written in such languages.                                           *|
 ## |*                                                                            *|
 ## \*===----------------------------------------------------------------------===
-## *
-##  @defgroup LLVMCAnalysis Analysis
-##  @ingroup LLVMC
-##
-##  @{
-##
+
+import prelude/platforms
+
+import Types
+
 
 type
     VerifierFailureAction* {.size: sizeof(cint).} = enum
@@ -29,20 +28,20 @@ type
         ReturnStatusAction  ##  verifier will just return 1
 
 
-##  Verifies that a module is valid, taking the specified action if not.
-##    Optionally returns a human-readable description of any invalid constructs.
-##    OutMessage must be disposed with LLVMDisposeMessage.
-
 proc verifyModule*(m: ModuleRef; action: VerifierFailureAction;outMessage: cstringArray): Bool {.cdecl, importc: "LLVMVerifyModule", dynlib: LLVMlib.}
-##  Verifies that a single function is valid, taking the specified action. Useful
-##    for debugging.
+    ##  Verifies that a module is valid, taking the specified action if not.
+    ##    Optionally returns a human-readable description of any invalid constructs.
+    ##    OutMessage must be disposed with LLVMDisposeMessage.
+
 
 proc verifyFunction*(fn: ValueRef; action: VerifierFailureAction): Bool {.cdecl, importc: "LLVMVerifyFunction", dynlib: LLVMlib.}
-##  Open up a ghostview window that displays the CFG of the current function.
-##    Useful for debugging.
+    ##  Verifies that a single function is valid, taking the specified action. Useful
+    ##    for debugging.
 
 proc viewFunctionCFG*(fn: ValueRef) {.cdecl, importc: "LLVMViewFunctionCFG", dynlib: LLVMlib.}
+    ##  Open up a ghostview window that displays the CFG of the current function.
+    ##  Useful for debugging.
+
 proc viewFunctionCFGOnly*(fn: ValueRef) {.cdecl, importc: "LLVMViewFunctionCFGOnly", dynlib: LLVMlib.}
-## *
-##  @}
-##
+    ##  Open up a ghostview window that displays the CFG of the current function.
+    ##  Useful for debugging.
