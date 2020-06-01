bc78c142c6dcac26e3b70501ef529138cb1b740a
chocobo333 <chocobo1355@gmail.com>
2020-06-01 16:06:49 +0900

    revise ErrorHandling.nim

--- src/llvm/ErrorHandling.nim
+++ src/llvm/ErrorHandling.nim
@@ -11,28 +11,24 @@
 ## |*                                                                            *|
 ## \*===----------------------------------------------------------------------===
 
+import prelude/platforms
+
+
 type
     FatalErrorHandler* = proc (reason: cstring) {.cdecl.}
 
-## *
-##  Install a fatal error handler. By default, if LLVM detects a fatal error, it
-##  will call exit(1). This may not be appropriate in many contexts. For example,
-##  doing exit(1) will bypass many crash reporting/tracing system tools. This
-##  function allows you to install a callback that will be invoked prior to the
-##  call to exit(1).
-##
-
 proc installFatalErrorHandler*(handler: FatalErrorHandler) {.cdecl, importc: "LLVMInstallFatalErrorHandler", dynlib: LLVMlib.}
-## *
-##  Reset the fatal error handler. This resets LLVM's fatal error handling
-##  behavior to the default.
-##
+    ##  Install a fatal error handler. By default, if LLVM detects a fatal error, it
+    ##  will call exit(1). This may not be appropriate in many contexts. For example,
+    ##  doing exit(1) will bypass many crash reporting/tracing system tools. This
+    ##  function allows you to install a callback that will be invoked prior to the
+    ##  call to exit(1).
 
 proc resetFatalErrorHandler*() {.cdecl, importc: "LLVMResetFatalErrorHandler", dynlib: LLVMlib.}
-## *
-##  Enable LLVM's built-in stack trace code. This intercepts the OS's crash
-##  signals and prints which component of LLVM you were in at the time if the
-##  crash.
-##
+    ##  Reset the fatal error handler. This resets LLVM's fatal error handling
+    ##  behavior to the default.
 
 proc enablePrettyStackTrace*() {.cdecl, importc: "LLVMEnablePrettyStackTrace", dynlib: LLVMlib.}
+    ##  Enable LLVM's built-in stack trace code. This intercepts the OS's crash
+    ##  signals and prints which component of LLVM you were in at the time if the
+    ##  crash.
