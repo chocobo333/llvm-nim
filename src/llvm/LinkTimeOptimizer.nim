f46fddfdec9145accb9433be15f496a35a1d3d9c
chocobo333 <chocobo1355@gmail.com>
2020-06-01 16:43:05 +0900

    revise LinkTimeOptimizer.nim

--- src/llvm/LinkTimeOptimizer.nim
+++ src/llvm/LinkTimeOptimizer.nim
@@ -11,39 +11,37 @@
 ##  their implementation for performing LTO.
 ##
 ## ===----------------------------------------------------------------------===//
-## *
-##  @defgroup LLVMCLinkTimeOptimizer Link Time Optimization
-##  @ingroup LLVMC
-##
-##  @{
-##
-## / This provides a dummy type for pointers to the LTO object.
+
+import prelude/platforms
+
 
 type
   LlvmLtoT* = pointer
+  ## / This provides a dummy type for pointers to the LTO object.
 
-## / This provides a C-visible enumerator to manage status codes.
-## / This should map exactly onto the C++ enumerator LTOStatus.
 
 type
-  LlvmLtoStatusT* {.size: sizeof(cint).} = enum
-    _LTO_UNKNOWN, _LTO_OPT_SUCCESS, _LTO_READ_SUCCESS, _LTO_READ_FAILURE,
-    _LTO_WRITE_FAILURE, _LTO_NO_TARGET, _LTO_NO_WORK, _LTO_MODULE_MERGE_FAILURE, _LTO_ASM_FAILURE, ##
-                                                                                              ## Added
-                                                                                              ## C-specific
-                                                                                              ## error
-                                                                                              ## codes
-    _LTO_NULL_OBJECT
+    LlvmLtoStatusT* {.size: sizeof(cint).} = enum
+        ## / This provides a C-visible enumerator to manage status codes.
+        ## / This should map exactly onto the C++ enumerator LTOStatus.
+        LTO_UNKNOWN
+        LTO_OPT_SUCCESS
+        LTO_READ_SUCCESS
+        LTO_READ_FAILURE
+        LTO_WRITE_FAILURE
+        LTO_NO_TARGET
+        LTO_NO_WORK
+        LTO_MODULE_MERGE_FAILURE
+        LTO_ASM_FAILURE             ## Added C-specific error codes
+        LTO_NULL_OBJECT
 
 
-## / This provides C interface to initialize link time optimizer. This allows
-## / linker to use dlopen() interface to dynamically load LinkTimeOptimizer.
-## / extern "C" helps, because dlopen() interface uses name to find the symbol.
 
 proc llvmCreateOptimizer*(): LlvmLtoT {.cdecl, importc: "llvm_create_optimizer", dynlib: LLVMlib.}
+    ## / This provides C interface to initialize link time optimizer. This allows
+    ## / linker to use dlopen() interface to dynamically load LinkTimeOptimizer.
+    ## / extern "C" helps, because dlopen() interface uses name to find the symbol.
+
 proc llvmDestroyOptimizer*(lto: LlvmLtoT) {.cdecl, importc: "llvm_destroy_optimizer", dynlib: LLVMlib.}
 proc llvmReadObjectFile*(lto: LlvmLtoT; inputFilename: cstring): LlvmLtoStatusT {. cdecl, importc: "llvm_read_object_file", dynlib: LLVMlib.}
 proc llvmOptimizeModules*(lto: LlvmLtoT; outputFilename: cstring): LlvmLtoStatusT {. cdecl, importc: "llvm_optimize_modules", dynlib: LLVMlib.}
-## *
-##  @}
-##
