47d6b1eef7c6bd6214fac45b028516e1ef5f8307
chocobo333 <chocobo1355@gmail.com>
2020-06-01 16:54:43 +0900

    change directory structure

---
+++ src/llvm/raw.nim
@@ -0,0 +1,63 @@
+
+const
+    LLVMRoot = "/usr/local/opt/llvm/"
+
+
+{.passC: "`llvm-config --cflags`".}
+{.passL: "`llvm-config --cflags --libs --ldflags all --system-libs`".}
+{.passL: "-lLLVM".}
+{.passL: "-Wl,-rpath " & LLVMRoot & "lib".}
+
+
+import llvm/[
+    Analysis,
+    BitReader,
+    BitWriter,
+    Comdat,
+    Core,
+    Debuginfo,
+    Disassembler,
+    DisassemblerTypes,
+    Error,
+    ErrorHandling,
+    ExecutionEngine,
+    ExternC,
+    Initialization,
+    IRReader,
+    Linker,
+    LinkTimeOptimizer,
+    lto,
+    Object,
+    OrcBindings,
+    Remarks,
+    Support,
+    Target,
+    TargetMachine,
+    Types
+]
+
+export
+    Analysis,
+    BitReader,
+    BitWriter,
+    Comdat,
+    Core,
+    Debuginfo,
+    Disassembler,
+    DisassemblerTypes,
+    Error,
+    ErrorHandling,
+    ExecutionEngine,
+    ExternC,
+    Initialization,
+    IRReader,
+    Linker,
+    LinkTimeOptimizer,
+    lto,
+    Object,
+    OrcBindings,
+    Remarks,
+    Support,
+    Target,
+    TargetMachine,
+    Types
