f54ccf4ed3d3d0ebdcab8ce562ffb811f1b9ddbd
chocobo333 <chocobo1355@gmail.com>
2020-06-01 16:15:10 +0900

    revise IRReader.nim

--- src/llvm/IRReader.nim
+++ src/llvm/IRReader.nim
@@ -10,14 +10,17 @@
 ## |* This file defines the C interface to the IR Reader.                        *|
 ## |*                                                                            *|
 ## \*===----------------------------------------------------------------------===
-## *
-##  Read LLVM IR from a memory buffer and convert it into an in-memory Module
-##  object. Returns 0 on success.
-##  Optionally returns a human-readable description of any errors that
-##  occurred during parsing IR. OutMessage must be disposed with
-##  LLVMDisposeMessage.
-##
-##  @see llvm::ParseIR()
-##
+
+import prelude/platforms
+
+import Types
+
 
 proc parseIRInContext*(contextRef: ContextRef; memBuf: MemoryBufferRef;outM: ptr ModuleRef; outMessage: cstringArray): Bool {.cdecl, importc: "LLVMParseIRInContext", dynlib: LLVMlib.}
+    ##  Read LLVM IR from a memory buffer and convert it into an in-memory Module
+    ##  object. Returns 0 on success.
+    ##  Optionally returns a human-readable description of any errors that
+    ##  occurred during parsing IR. OutMessage must be disposed with
+    ##  LLVMDisposeMessage.
+    ##
+    ##  @see llvm::ParseIR()
