216b86ab618c977efa570a1a11b5f060b7d3304b
chocobo333 <chocobo1355@gmail.com>
2020-06-01 16:42:52 +0900

    revise Linker.nim

--- src/llvm/Linker.nim
+++ src/llvm/Linker.nim
@@ -10,18 +10,20 @@
 ## |* This file defines the C interface to the module/file/archive linker.       *|
 ## |*                                                                            *|
 ## \*===----------------------------------------------------------------------===
-##  This enum is provided for backwards-compatibility only. It has no effect.
+
+import prelude/platforms
+
+import Types
+
 
 type
     LinkerMode* {.size: sizeof(cint).} = enum
+        ##  This enum is provided for backwards-compatibility only. It has no effect.
         LinkerDestroySource = 0, ##  This is the default behavior.
         LinkerPreserveSourceRemoved = 1
 
-
-##  Links the source module into the destination module. The source module is
-##  destroyed.
-##  The return value is true if an error occurred, false otherwise.
-##  Use the diagnostic handler to get any diagnostic message.
-##
-
 proc linkModules2*(dest: ModuleRef; src: ModuleRef): Bool {.cdecl, importc: "LLVMLinkModules2", dynlib: LLVMlib.}
+    ##  Links the source module into the destination module. The source module is
+    ##  destroyed.
+    ##  The return value is true if an error occurred, false otherwise.
+    ##  Use the diagnostic handler to get any diagnostic message.
