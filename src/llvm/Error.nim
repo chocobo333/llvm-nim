a1bc27efcf80c753026b39881cd54af271165d9d
chocobo333 <chocobo1355@gmail.com>
2020-06-01 16:05:39 +0900

    revise Error.nim

--- src/llvm/Error.nim
+++ src/llvm/Error.nim
@@ -10,49 +10,41 @@
 ## |* This file defines the C interface to LLVM's Error class.                   *|
 ## |*                                                                            *|
 ## \*===----------------------------------------------------------------------===
-## *
-##  Opaque reference to an error instance. Null serves as the 'success' value.
-##
+
+import prelude/[
+    opaques,
+    platforms
+]
+
 
 type
     ErrorRef* = ptr OpaqueError
+    ##  Opaque reference to an error instance. Null serves as the 'success' value.
 
-## *
-##  Error type identifier.
-##
 
 type
     ErrorTypeId* = pointer
+    ##  Error type identifier.
 
-## *
-##  Returns the type id for the given error instance, which must be a failure
-##  value (i.e. non-null).
-##
 
 proc getErrorTypeId*(err: ErrorRef): ErrorTypeId {.cdecl, importc: "LLVMGetErrorTypeId", dynlib: LLVMlib.}
-## *
-##  Dispose of the given error without handling it. This operation consumes the
-##  error, and the given LLVMErrorRef value is not usable once this call returns.
-##  Note: This method *only* needs to be called if the error is not being passed
-##  to some other consuming operation, e.g. LLVMGetErrorMessage.
-##
+    ##  Returns the type id for the given error instance, which must be a failure
+    ##  value (i.e. non-null).
 
 proc consumeError*(err: ErrorRef) {.cdecl, importc: "LLVMConsumeError", dynlib: LLVMlib.}
-## *
-##  Returns the given string's error message. This operation consumes the error,
-##  and the given LLVMErrorRef value is not usable once this call returns.
-##  The caller is responsible for disposing of the string by calling
-##  LLVMDisposeErrorMessage.
-##
+    ##  Dispose of the given error without handling it. This operation consumes the
+    ##  error, and the given LLVMErrorRef value is not usable once this call returns.
+    ##  Note: This method *only* needs to be called if the error is not being passed
+    ##  to some other consuming operation, e.g. LLVMGetErrorMessage.
 
 proc getErrorMessage*(err: ErrorRef): cstring {.cdecl, importc: "LLVMGetErrorMessage", dynlib: LLVMlib.}
-## *
-##  Dispose of the given error message.
-##
+    ##  Returns the given string's error message. This operation consumes the error,
+    ##  and the given LLVMErrorRef value is not usable once this call returns.
+    ##  The caller is responsible for disposing of the string by calling
+    ##  LLVMDisposeErrorMessage.
 
 proc disposeErrorMessage*(errMsg: cstring) {.cdecl, importc: "LLVMDisposeErrorMessage", dynlib: LLVMlib.}
-## *
-##  Returns the type id for llvm StringError.
-##
+    ##  Dispose of the given error message.
 
 proc getStringErrorTypeId*(): ErrorTypeId {.cdecl, importc: "LLVMGetStringErrorTypeId", dynlib: LLVMlib.}
+    ##  Returns the type id for llvm StringError.
